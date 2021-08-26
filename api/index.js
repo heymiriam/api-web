6d321e9347bc321f970e9d3981ec8e62102e2703
heymiriam <miriambellon@gmail.com>
2021-08-26 12:52:13 -0700

    correct port

--- api/index.js
+++ api/index.js
@@ -18,9 +18,7 @@ app.get('/api/:id', function (req, res, next) {
     res.json({msg: 'This is CORS-enabled for all origins!'})
   })
    
-  app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-  })
+ 
 app.use("/assets", express.static(path.join(__dirname, "/assets")));
 
 

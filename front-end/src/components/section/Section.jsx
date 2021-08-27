4cc39fafbf50a32c4bc32a4882420763a70c7037
heymiriam <miriambellon@gmail.com>
2021-08-27 08:25:37 -0700

    correct

--- front-end/src/components/section/Section.jsx
+++ front-end/src/components/section/Section.jsx
@@ -18,10 +18,10 @@ function Section(){
     return(
         <div className="section">
             <div className="section-item">
-                <p className="section-heading">About Me</p>
+                <p className="section-heading">Coffee Time</p>
                 <img className="section-img" src="https://images.unsplash.com/photo-1518057111178-44a106bad636?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80">
                 </img>
-                <p>dummy text</p>
+                <p>Welcome to my blog</p>
             </div> 
             <div className="section-item">
                 <p className="section-heading">Categories</p>
